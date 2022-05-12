e64b437ee23b8e6e4dcade33e906062eff4442c1
wjdals819 <wjdals819@github.com>
2022-05-13 00:57:42 +0900

    Brush 메뉴 추가

---
+++ resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCTeamProject.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCTeamProjectTYPE          130
+#define ID_BRUSH_TYPE                   32771
+#define ID_BRUSH_SIZE                   32772
+#define ID_SIZE_1                       32773
+#define ID_SIZE_3                       32774
+#define ID_SIZE_5                       32775
+#define ID_SIZE_10                      32776
+#define ID_SIZE_20                      32777
+#define ID_TYPE_SOLID                   32778
+#define ID_TYPE_DASH                    32779
+#define ID_TYPE_DOT                     32780
+#define ID_TYPE_DASHDOT                 32781
+#define ID_TYPE_DASHDOTDOT              32782
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
